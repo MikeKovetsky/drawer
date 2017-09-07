2415d0ad61409cf9bf3528c44b598ebb1dcb6169
Mike Kovetsky Asus <praskovyaseasons@gmail.com>
2017-09-07 22:23:24 +0300

    angular init

--- src/old/main.js
+++ src/old/main.js
@@ -5,12 +5,12 @@ class Canvas {
 		this.height = height;
 		this.vectorLength = vectorLength;	
 		this.canvas = document.getElementById('canvas');
-		canvas.setAttribute('width', width);
-		canvas.setAttribute('height', height);
+		this.canvas.setAttribute('width', width);
+        this.canvas.setAttribute('height', height);
 	}
 	getContext() {
 		if (this.canvas.getContext) {
-			return canvas.getContext('2d');
+			return this.canvas.getContext('2d');
 		} else {
 			alert('Browser is too old');
 		}
@@ -46,13 +46,23 @@ class Drawer {
 	}
 	initAxis() {
 		this.context.globalAlpha = 1;
-		//x and y lines
+		//x and y axis
 		this.drawLine(new Point(0, this.canvas.height / 2) , new Point(this.canvas.width, this.canvas.height / 2));	
-		this.drawLine(new Point(this.canvas.width / 2, 0) , new Point(this.canvas.width / 2, this.canvas.height));	
+		this.drawLine(new Point(this.canvas.width / 2, 0) , new Point(this.canvas.width / 2, this.canvas.height));
+        for (let i = 0; i < this.canvas.height; i += this.canvas.vectorLength) {
+            const helperStrokeStart = new Point(this.canvas.width / 2 - this.canvas.vectorLength / 2, i);
+            const helperStrokeEnd = new Point(this.canvas.width / 2 + this.canvas.vectorLength / 2, i);
+            this.drawLine(helperStrokeStart, helperStrokeEnd);
+        }
+        for (let i = 0; i < this.canvas.width; i += this.canvas.vectorLength) {
+            const helperStrokeStart  = new Point(i, this.canvas.height / 2 - this.canvas.vectorLength / 2);
+            const helperStrokeEnd  = new Point(i, this.canvas.height / 2 + this.canvas.vectorLength / 2);
+            this.drawLine(helperStrokeStart , helperStrokeEnd);
+        }
 		// set the canvas origin (0,0) to center canvas
 		// All coordinates to the left of center canvas are negative
 		// All coordinates below center canvas are negative
-		this.context.translate(canvas.width/2,canvas.height/2);
+		this.context.translate(this.canvas.width/2, this.canvas.height/2);
 	}
 
 	drawLine(p1, p2) {
@@ -64,4 +74,4 @@ class Drawer {
 }
 
 const drawer = new Drawer(new Canvas(1000, 600, 20));
-drawer.drawLine(new Point(0,0), new Point(220,220));
+drawer.drawLine(new Point(0,0), new Point(220,220)); // test line
