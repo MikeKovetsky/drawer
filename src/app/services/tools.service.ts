2542220e69ff38263cadc60dde3c393530379e7b
mkovetsky <mkovetsky@gmail.com>
2018-03-02 21:00:11 +0200

    header modes fix

--- src/app/services/tools.service.ts
+++ src/app/services/tools.service.ts
@@ -2,7 +2,22 @@ import {Injectable} from '@angular/core';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 import {Observable} from 'rxjs/Observable';
 
-  export type TOOL_PANEL = 'anchor' | 'transformations' | 'specific';
+export type TOOL_PANEL = 'anchor' | 'transformations' | 'specific';
+
+@Injectable()
+export class ToolsService {
+  splitMode = new Tool();
+  chainMode = new Tool();
+  private _openedPanel: TOOL_PANEL;
+
+  set openedPanel(pane: TOOL_PANEL) {
+    this._openedPanel = pane === this._openedPanel ? null : pane;
+  }
+  get openedPanel() {
+    return this._openedPanel;
+  }
+}
+
 
 class Tool {
   private _tool: BehaviorSubject<boolean>;
@@ -24,19 +39,5 @@ class Tool {
 
   toggle() {
     this._tool.next(!this._tool.value);
-  }
-}
-
-@Injectable()
-export class ToolsService {
-  splitMode = new Tool();
-  chainMode = new Tool();
-  private _openedPanel: TOOL_PANEL;
-
-  set openedPanel(pane: TOOL_PANEL) {
-    this._openedPanel = pane === this._openedPanel ? null : pane;
-  }
-  get openedPanel() {
-    return this._openedPanel;
   }
 }
